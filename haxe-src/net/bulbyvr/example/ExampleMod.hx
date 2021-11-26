02f19c81c21a89c611e0415821c696f86507d02b
BulbyVR <bulbyvr@gmail.com>
2021-11-26 11:08:51 -0500

    small edit

--- haxe-src/net/bulbyvr/example/ExampleMod.hx
+++ haxe-src/net/bulbyvr/example/ExampleMod.hx
@@ -2,6 +2,7 @@ package net.bulbyvr.example;
 import net.fabricmc.api.ModInitializer;
 
 class ExampleMod implements ModInitializer {
+    public function new() {}
     public function onInitialize():Void {
         Sys.println("Hello Fabric!");
     }
