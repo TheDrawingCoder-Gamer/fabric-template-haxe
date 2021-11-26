1e4709fd61e863f2f5ad46a2024c43e3ce418c79
BulbyVR <bulbyvr@gmail.com>
2021-11-26 11:04:50 -0500

    oopsies

--- compile.bat
+++ compile.bat
@@ -8,5 +8,6 @@ haxe compile.hxml
 xcopy /E /I "resources/." "outjar"
 del out.jar
 cd outjar
-jar cf ../out.jar **/**
+jar cf ../out.jar **/** .
+cd ..
 ./gradlew.bat build
--- compile.sh
+++ compile.sh
@@ -8,9 +8,10 @@ haxe compile.hxml
 cp -r resources/. outjar
 rm out.jar
 cd outjar
-jar cf ../out.jar **/**
+jar cf ../out.jar ./**/** .
 # lol 
 # No copying because we don't compile to java anymore
 # rm -rf src/main/java
 # cp -rf bin/src src/main/java
+cd ..
 ./gradlew build
