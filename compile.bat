08c468b7035ae9c8d0c60d7ea24b7c0e16bb2984
BulbyVR <bulbyvr@gmail.com>
2021-11-26 10:40:05 -0500

    (untested) windows compat

--- compile.bat
+++ compile.bat
@@ -2,6 +2,10 @@ rmdir "javalibs" /Q /S
 ./gradlew.bat getDeps
 haxe --run BuildFromDirectory
 haxe compile.hxml
+./extractjar.bat
 :: rmdir "src/main/java" /Q /S
 :: xcopy /E /I "bin/src" "src/main/java" 
+xcopy /E /I "resources/." "outjar"
+del out.jar
+jar cf out.jar outjar/.
 ./gradlew.bat build
---
+++ extractjar.bat
@@ -0,0 +1,4 @@
+if exist "./outjar" rmdir "./outjar" /Q /S
+mkdir "./outdir"
+cd outjar
+jar xf ../out.jar
