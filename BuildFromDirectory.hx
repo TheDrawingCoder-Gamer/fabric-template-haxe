6f930f9ff46bb688a7d14ad95e378495d0947ca3
BulbyVR <bulbyvr@gmail.com>
2021-11-30 13:32:14 -0500

    make nicer to work with : )

--- BuildFromDirectory.hx
+++ BuildFromDirectory.hx
@@ -3,10 +3,11 @@ import haxe.io.Path;
 import sys.FileSystem;
 using StringTools;
 class BuildFromDirectory {
+    static final TOP_LEVEL_PACKAGE = "net";
     public static function main() {
         // we only want .net
         var dir = FileSystem.readDirectory('javalibs');
-        var stinkyFile:Array<String> = ["--class-path src", "net", "-L hxjava", "--java-lib-extern javax/javax-crypto.jar"];
+        var stinkyFile:Array<String> = ["--class-path src", TOP_LEVEL_PACKAGE, "-L hxjava", "--java-lib-extern javax/javax-crypto.jar"];
         for (thing in dir) {
             if (Path.extension(thing)  == 'jar') {
                 stinkyFile.push("--java-lib-extern javalibs/" + thing);
