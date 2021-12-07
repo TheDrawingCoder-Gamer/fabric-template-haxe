11b8c92019fa1426643df541b9e8c2024f8685e3
BulbyVR <bulbyvr@gmail.com>
2021-12-07 08:59:28 -0500

    glue try fixie

--- src/net/fabricmc/example/mixin/ExampleMixin.hx
+++ src/net/fabricmc/example/mixin/ExampleMixin.hx
@@ -2,9 +2,10 @@ package net.fabricmc.example.mixin;
 
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.fabricmc.example.ExampleMod;
-@:build(fabric.glue.Glue.mixin(net.minecraft.client.gui.screen.TitleScreen))
+
+@:mixin(net.minecraft.client.gui.screen.TitleScreen)
 class ExampleMixin {
-    @inject("HEAD", "init()V")
+    @:inject(at = "HEAD", method = "init()V")
     public static function init(info:CallbackInfo) {
         ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
     }
